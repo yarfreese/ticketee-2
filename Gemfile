bda210c23bf52c79045a440d2899e00f6ed96bc5
Theresa Solak <yarfreese@gmail.com>
2013-05-19 09:39:37 -0700

    Add the ability to attach a file to a ticket

--- Gemfile
+++ Gemfile
@@ -59,4 +59,5 @@ end
 
 gem 'devise', '2.1.0'
 gem 'cancan', '1.6.7'
+gem 'paperclip', '2.7.0'
 
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,8 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
-  attr_accessible :description, :title
+  has_attached_file :asset
+  attr_accessible :description, :title, :asset
   validates :title, :presence => true
   validates :description, :presence => true, :length => { :minimum => 10 }
 end
